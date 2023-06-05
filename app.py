3bdf69988291c72160c12032f47ef422724da35d
Joseph Perez <perezj0230@gmail.com>
2023-06-05 15:09:29 -0400

    add migration for user and user logs

--- app.py
+++ app.py
@@ -9,29 +9,34 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
-migrate = Migrate(app, db) 
+migrate = Migrate(app, db)
 
-from views.seed import seed_blueprint
 from views.food import food_blueprint
+from views.seed import seed_blueprint
 app.register_blueprint(seed_blueprint)
-app.register_blueprint(food_blueprint, url_prefix = '/food')
+app.register_blueprint(food_blueprint, url_prefix='/food')
+
 
 @app.route('/')
-def hello():    
+def hello():
     return "Hello world!"
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "404 Not Found", 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return "500 Internal Server Error", 500
+
 
 @app.errorhandler(exc.SQLAlchemyError)
 def sql_alchemy_error(e):
     print(e)
     return "500 SQLAlchemy Error", 500
+
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from app import db, app
+import datetime
+
 
 @dataclass
 class Restaurant(db.Model):
@@ -9,10 +11,12 @@ class Restaurant(db.Model):
 
     # Define columns for database
     id = db.Column(db.INTEGER(), primary_key=True)
-    name = db.Column(db.String(255), nullable=False) # restaurant must have a name
+    # restaurant must have a name
+    name = db.Column(db.String(255), nullable=False)
 
-    def __repr__(self): # used by print()
+    def __repr__(self):  # used by print()
         return '<User id={id}, name={id}>'.format(id=self.id, name=self.name)
+
 
 @dataclass
 class FoodItem(db.Model):
@@ -35,5 +39,35 @@ class FoodItem(db.Model):
     fiber = db.Column(db.INTEGER())
     protein = db.Column(db.INTEGER())
 
-    def __repr__(self): # used by print()
+    def __repr__(self):  # used by print()
         return '<FoodItem id={id}, name={id}>'.format(id=self.id, name=self.name)
+
+
+@dataclass
+class User(db.Model):
+   # Define dataclass (to convert object to JSON)
+    id: int
+    name: str
+    email: str
+
+    id = db.Column(db.INTEGER(), primary_key=True)
+    name = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    email = db.Column(db.String(255))
+
+    def __repr__(self):  # used by print()
+        return '<User id={id}, name={name}>'.format(id=self.id, name=self.name)
+
+
+@dataclass
+class FoodLog(db.Model):
+    id: int
+    user_id: int
+    food_item_id: int
+    created_at: datetime
+
+    id = db.Column(db.INTEGER(), primary_key=True)
+    user_id = db.Column(db.ForeignKey('user.id'), nullable=False)
+    food_item_id = db.Column(db.ForeignKey('food_item.id'), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           server_default=db.func.now())
