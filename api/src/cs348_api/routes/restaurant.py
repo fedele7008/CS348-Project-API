1d01ce2a6aa6a2ec38c409e21d8b6b2ccbbda0b9
Stephanie Xu <stephxca@gmail.com>
2023-06-21 15:22:49 -0400

    add code for r6 and update readme

--- api/src/cs348_api/routes/restaurant.py
+++ api/src/cs348_api/routes/restaurant.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 
 from cs348_api.extensions import db
 from cs348_api.models.restaurant import Restaurant
+from cs348_api.models.food_item import FoodItem
 
 restaurant = Blueprint('restaurant', __name__, url_prefix='/restaurant')
 
@@ -10,3 +11,10 @@ restaurant = Blueprint('restaurant', __name__, url_prefix='/restaurant')
 def get_restaurants():
     records = db.session.query(Restaurant).all()
     return jsonify(records)
+
+@restaurant.route('/<string:name>/foods', methods=['GET'])
+def get_restaurant_foods(name):
+    foods = db.session.query(FoodItem)\
+        .join(Restaurant, FoodItem.restaurant_id==Restaurant.id)\
+        .filter(Restaurant.name == name).all()
+    return jsonify(foods)
