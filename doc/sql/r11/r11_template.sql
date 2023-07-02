8088b9962adc7acaa1f91ebe05dc0e7581231e22
Seungjae <s34yoo@uwaterloo.ca>
2023-07-01 21:41:26 -0400

    r11 sql

---
+++ doc/sql/r11/r11_template.sql
@@ -0,0 +1,30 @@
+# 
+WITH T1 as (
+    SELECT user_id, sum(calories) as calories, sum(fat) as fat, sum(carb) as carb, sum(fiber) as fiber, sum(protein) as protein
+    FROM food_log
+    INNER JOIN food_item on food_item.id = food_log.food_item_id
+    WHERE user_id = <User ID>
+    AND DATE(created_at) = CURDATE()
+    GROUP BY user_id
+),
+
+T2 as (
+    SELECT T1.user_id, goal.name, goal.goal_type, goal.quantity,
+    CASE 
+    WHEN goal.goal_type = 'Calorie' THEN T1.calories 
+    WHEN goal.goal_type = 'Fat' THEN T1.fat
+    WHEN goal.goal_type = 'Sugar' THEN T1.carb
+    WHEN goal.goal_type = 'Fiber' THEN T1.fiber
+    WHEN goal.goal_type = 'Protein' THEN T1.protein
+    END as consumption
+    FROM goal INNER JOIN T1 on T1.user_id = goal.user_id
+    WHERE goal.user_id = <User ID>
+)
+
+SELECT name, goal_type, quantity, consumption,
+    CASE
+    WHEN consumption > quantity THEN 'Goal Achieved'
+    ELSE 'Goal not Achieved'
+    END as status
+    from T2;
+
