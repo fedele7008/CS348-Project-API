4b919cdc29824a9e0fb5497394e81fa4782aa087
Seungjae <s34yoo@uwaterloo.ca>
2023-07-09 20:40:09 -0400

    improved sql queires

---
+++ doc/sql/r6/r6_improved.sql
@@ -0,0 +1,14 @@
+SELECT food_item.name, calories, fat, carb, fiber, protein
+FROM food_item 
+WHERE EXISTS (
+    SELECT 1 
+    FROM restaurant 
+    WHERE food_item.restaurant_id = restaurant.id 
+    AND restaurant.name = <Restaurant Name>
+); 
+
+SELECT name, calories, fat, carb, fiber, protein
+FROM food_item 
+WHERE name = <foodName>;
+
+
