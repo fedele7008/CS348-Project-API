c51a80e5aecdbff354fe428cde5911868733db31
Joseph Perez <perezj0230@gmail.com>
2023-07-10 17:30:38 -0400

    food logging api

--- api/src/cs348_api/routes/food_log.py
+++ api/src/cs348_api/routes/food_log.py
@@ -3,19 +3,60 @@ from flask import Blueprint, jsonify, request
 from cs348_api.extensions import db
 from cs348_api.models.food_log import FoodLog
 from cs348_api.models.user import User
-from cs348_api.models.food_item import FoodItem 
+from cs348_api.models.food_item import FoodItem
+
+from datetime import datetime
 
 food_log = Blueprint('food_log', __name__, url_prefix='/log')
 
-
-@food_log.route('/', methods=['GET'])
+@food_log.route('/', methods=['GET', 'POST'])
 def get_food_logs():
-    logs = db.session.query(FoodLog)\
-        .join(FoodItem, FoodLog.food_item_id == FoodItem.id)\
-        .join(User, FoodLog.user_id == User.id).all()
+    if request.method == "GET":
+        logs = db.session.query(FoodLog, FoodItem)\
+            .join(FoodItem, FoodLog.food_item_id == FoodItem.id)\
+            .join(User, FoodLog.user_id == User.id).all()
+        logs = [tuple(row) for row in logs]
+        return jsonify(logs), 200
+    elif request.method == "POST":
+        # Get information from request
+        if not request.json:
+            return jsonify({
+                'result': 'Invalid request',
+                'message': 'Request must be in JSON format'
+            }), 400
+        
+        user_id = request.json.get('user_id')
+        food_item_id = request.json.get('food_item_id')
+        date = request.json.get('date')
+        created_at = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%fZ")
 
-    return jsonify(logs), 200
+        if user_id is None:
+            return jsonify({
+                'result': 'Invalid request',
+                'message': 'User ID is required'
+            }), 400
+        
+        if food_item_id is None:
+            return jsonify({
+                'result': 'Invalid request',
+                'message': 'Food Item ID is required'
+            }), 400
+        
+        if date is None:
+            return jsonify({
+                'result': 'Invalid request',
+                'message': 'Date is required'
+            }), 400 
 
+        food_log = FoodLog(user_id=user_id, food_item_id=food_item_id, created_at=created_at)
+        db.session.add(food_log)
+        db.session.commit()
+
+        # Return response
+        return jsonify({
+            'result': 'Food log created',
+            'message': 'Added food log: {}'.format(food_log)
+        }), 201
 
 @food_log.route('/<int:id>', methods=['GET'])
 def get_food_log(id):
@@ -35,42 +76,6 @@ def get_food_logs_of(user_id):
         .filter(User.id == user_id).all()
 
     return jsonify(logs), 200
-
-
-@food_log.route('/', methods=['POST'])
-def add_food_log():
-    # Get information from request
-    if not request.json:
-        return jsonify({
-            'result': 'Invalid request',
-            'message': 'Request must be in JSON format'
-        }), 400
-    
-    user_id = request.json.get('user_id')
-    food_item_id = request.json.get('food_item_id')
-
-    if user_id is None:
-        return jsonify({
-            'result': 'Invalid request',
-            'message': 'User ID is required'
-        }), 400
-    
-    if food_item_id is None:
-        return jsonify({
-            'result': 'Invalid request',
-            'message': 'Food Item ID is required'
-        }), 400
-    
-    food_log = FoodLog(user_id=user_id, food_item_id=food_item_id)
-    db.session.add(food_log)
-    db.session.commit()
-
-    # Return response
-    return jsonify({
-        'result': 'Food log created',
-        'message': 'Added food log: {}'.format(food_log)
-    }), 201
-
 
 @food_log.route('/<int:id>', methods=['PUT'])
 def update_food_log(id):
